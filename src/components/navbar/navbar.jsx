9d58ab05cd59ac35dc6352a40f9a82227a3e1061
GaryChettiar <101002917+GaryChettiar@users.noreply.github.com>
2025-02-28 18:23:12 +0530

    first commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar = ({ scrollToSection, homeRef, aboutRef, skillsRef, projectsRef,eduRef ,contactRef}) => {
   const [activeTab, setActiveTab] = useState("Home");
